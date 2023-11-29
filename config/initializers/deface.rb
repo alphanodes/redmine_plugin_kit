478c99816dfbff1665b6eb42017a4836f7e861bb
Alexander Meindl <a.meindl@alphanodes.com>
2023-11-29 18:26:52 +0100

    add dsl loader for deface

--- config/initializers/deface.rb
+++ config/initializers/deface.rb
@@ -4,3 +4,7 @@ Rails.application.paths['app/overrides'] ||= []
 Dir[Rails.root.join('plugins/*/app/overrides')].each do |dir|
   Rails.application.paths['app/overrides'] << dir unless Rails.application.paths['app/overrides'].include? dir
 end
+
+Rails.root.glob('plugins/*/app/overrides/**/*.deface').each do |path|
+  Deface::DSL::Loader.load File.expand_path(path, __FILE__)
+end
