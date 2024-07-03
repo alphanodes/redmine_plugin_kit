938884a70079c5939b4ac871584f3dd7061cea38
Alexander Meindl <a.meindl@alphanodes.com>
2024-07-03 17:21:27 +0200

    cosmetics

--- test/schema.rb
+++ test/schema.rb
@@ -2,17 +2,17 @@
 
 ActiveRecord::Schema.define version: 0 do
   create_table 'users', force: true do |t|
-    t.column 'name', :string
-    t.column 'language', :string
+    t.string 'name'
+    t.string 'language'
   end
 
   create_table 'issues', force: true do |t|
     t.integer  'project_id'
-    t.column 'subject', :string
-    t.column 'description', :string
-    t.column 'closed', :boolean
-    t.column 'user_id', :integer
-    t.column 'author_id', :integer
+    t.string 'subject'
+    t.string 'description'
+    t.boolean 'closed'
+    t.integer 'user_id'
+    t.integer 'author_id'
   end
 
   create_table 'projects', force: true do |t|
